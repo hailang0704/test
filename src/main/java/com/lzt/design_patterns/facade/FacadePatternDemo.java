2ed9aabf5146152040b7343a77c179f189088494
lizhitao <lizhitao@myhexin.com>
2018-05-14 09:14:28 +0800

    add 提交外观设计模式代码

---
+++ src/main/java/com/lzt/design_patterns/facade/FacadePatternDemo.java
@@ -0,0 +1,14 @@
+package com.lzt.design_patterns.facade;
+
+/**
+ * Created by viruser on 05/14.
+ */
+public class FacadePatternDemo {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
