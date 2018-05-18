01678c2a8efab5227efe12682a01349bf6aca4e1
lizhitao <lizhitao@myhexin.com>
2018-05-18 18:45:26 +0800

    add 提交原型模型设计模式代码

--- src/main/java/com/lzt/design_patterns/prototype/Shape.java
+++ src/main/java/com/lzt/design_patterns/prototype/Shape.java
@@ -2,30 +2,30 @@ package com.lzt.design_patterns.prototype;
 
 public abstract class Shape implements Cloneable {
 
-	private String id;
-	protected String type;
+    private String id;
+    protected String type;
 
-	abstract void draw();
+    abstract void draw();
 
-	public String getType() {
-		return type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public Object clone() {
-		Object clone = null;
-		try {
-			clone = super.clone();
-		} catch (CloneNotSupportedException e) {
-			e.printStackTrace();
-		}
-		return clone;
-	}
+    public Object clone() {
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
 }
--- src/main/java/com/lzt/design_patterns/prototype/Square.java
+++ src/main/java/com/lzt/design_patterns/prototype/Square.java
@@ -2,12 +2,12 @@ package com.lzt.design_patterns.prototype;
 
 public class Square extends Shape {
 
-	public Square() {
-		type = "Square";
-	}
+    public Square() {
+        type = "Square";
+    }
 
-	@Override
-	public void draw() {
-		System.out.println("Inside Square::draw() method.");
-	}
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
 }
