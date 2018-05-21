d3ef3eff808ba66be3d5d5c03252db425528dedd
lizhitao <lizhitao@myhexin.com>
2018-05-21 09:42:29 +0800

    add 提交备忘录设计模式代码

---
+++ src/main/java/com/lzt/design_patterns/memento/CareTaker.java
@@ -0,0 +1,19 @@
+package com.lzt.design_patterns.memento;
+
+/**
+ * Created by viruser on 05/21.
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class CareTaker {
+    private List<Memento> mementoList = new ArrayList<Memento>();
+
+    public void add(Memento state){
+        mementoList.add(state);
+    }
+
+    public Memento get(int index){
+        return mementoList.get(index);
+    }
+}
