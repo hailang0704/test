d5414bd348eb4e0adf8fa54c407332a8019a7c88
lizhitao <lizhitao@myhexin.com>
2018-05-21 19:05:13 +0800

    add 提交备忘录设计模式代码

---
+++ src/main/java/com/lzt/design_patterns/observer/ObserverPatternDemo.java
@@ -0,0 +1,20 @@
+package com.lzt.design_patterns.observer;
+
+/**
+ * Created by viruser on 05/21.
+ * subject持有各种观察者，setState时会通知所有观察者
+ */
+public class ObserverPatternDemo {
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+
+        new HexaObserver(subject);
+        new OctalObserver(subject);
+        new BinaryObserver(subject);
+
+        System.out.println("First state change: 15");
+        subject.setState(15);
+        System.out.println("Second state change: 10");
+        subject.setState(10);
+    }
+}
