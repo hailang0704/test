c36db524b1e89c6c7dbda827e9c9682c36c205ee
lizhitao <lizhitao@myhexin.com>
2018-05-22 09:46:05 +0800

    add 提交策略设计模式代码

---
+++ src/main/java/com/lzt/design_patterns/strategy/Context.java
@@ -0,0 +1,16 @@
+package com.lzt.design_patterns.strategy;
+
+/**
+ * Created by viruser on 05/22.
+ */
+public class Context {
+    private Strategy strategy;
+
+    public Context(Strategy strategy){
+        this.strategy = strategy;
+    }
+
+    public int executeStrategy(int num1, int num2){
+        return strategy.doOperation(num1, num2);
+    }
+}
---
+++ src/main/java/com/lzt/design_patterns/strategy/Strategy.java
@@ -0,0 +1,8 @@
+package com.lzt.design_patterns.strategy;
+
+/**
+ * Created by viruser on 05/22.
+ */
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
