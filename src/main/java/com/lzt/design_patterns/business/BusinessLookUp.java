ccdc7c9bc4011b824dd95a460760861f6b4dc23b
lizhitao <lizhitao@myhexin.com>
2018-05-24 09:29:25 +0800

    add 提交业务代表设计模式代码

---
+++ src/main/java/com/lzt/design_patterns/business/BusinessLookUp.java
@@ -0,0 +1,14 @@
+package com.lzt.design_patterns.business;
+
+/**
+ * Created by viruser on 05/24.
+ */
+public class BusinessLookUp {
+    public BusinessService getBusinessService(String serviceType){
+        if(serviceType.equalsIgnoreCase("EJB")){
+            return new EJBService();
+        }else {
+            return new JMSService();
+        }
+    }
+}
