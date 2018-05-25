64f57be3a117eee737f70122f732842e3dbb727d
lizhitao <lizhitao@myhexin.com>
2018-05-25 09:21:07 +0800

    add 提交组合实体设计模式代码

---
+++ src/main/java/com/lzt/design_patterns/composite/DependentObject1.java
@@ -0,0 +1,17 @@
+package com.lzt.design_patterns.composite;
+
+/**
+ * Created by viruser on 05/25.
+ */
+public class DependentObject1 {
+
+    private String data;
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+    public String getData(){
+        return data;
+    }
+}
